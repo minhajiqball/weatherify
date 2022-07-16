c1262339babe8fe3e0e5c6a7650b2bf278f4fbdb
Minhajiqball <minhajbiniqbal@gmail.com>
2022-07-16 16:29:38 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b127a3e9cbmshbca8165a177bc72p14a050jsnac2db1ee296b",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0eb16e782c68b4580b1cc415f243be15"; 
